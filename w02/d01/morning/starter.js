c3a5db5fc8c85365beca94d130f08892f2de3579
Jason Seminara <jseminara@gmail.com>
2016-09-26 12:57:50 -0400

    added d2 morning exercise/practice

--- w02/d01/morning/starter.js
+++ w02/d01/morning/starter.js
@@ -1,100 +1,106 @@
 
 //part 1:
 
+
+
 //1a
-function kanyesGoldenUnicorn(){console.log('wanted a plantinum unicorn');}
+function kanyesGoldenUnicorn() {
+  console.log('wanted a plantinum unicorn');
+}
+
+// as a function expression (with semicolon)
+var kanyesGoldenUnicorn = function() {
+  console.log('wanted a plantinum unicorn');
+};
 
 //1b
-var items = new Array();
+var items = [];
 
 //1c
 
-var big-animal-array = [
-  "elephant"
-  , "tiger"
-  , "bunny"
-  , "unicorn"
-  , "polar bear"
-]
+var bigAnimalArray = [
+  'elephant',
+  'tiger',
+  'bunny',
+  'unicorn',
+  'polar bear',
+];
 
 
 
 //part 2:
 
-var pickle = 22
+var pickle = 22;
+var cat = Math.random()*10;
 
-if(9 < var cat = Math.random()){console.log('Found the cat!')};;
-else if (pickle < 4){ console.log('AAHRHW')}
+if (cat > 9) {
+  console.log('Found the cat!');
+} else if (pickle < 4) {
+  console.log('AAHRHW');
+}
 
 
 //part 3:
+function login() {
+  function hideContainer(){
+    console.log('dummy hidden container');
+  }
+  $status = document.getElementById('status');
+  // we're assuming there was an HTTP call here, that returned a 'response'
+  if (response.status === 'connected') {
+    $status.innerHTML = "we are connected";
 
+  } else if (response.status === 'not_authorized') {
+    $status.innerHTML = "Not connected";
+    hideContainer();
 
-  function login() {
+  } else {
+    document.getElementById('status').innerHTML = "You are not logged in into facebook, please log in to take a quiz";
 
-if (response.status === 'connected') {
-document.getElementById('status').innerHTML = "we are con"
-
+  }
 }
-
-else if (response.status === 'not_authorized') {
-  document.getElementById('status').innerHTML = "Not connected";
-   hideContainer();
-
-}
-else {
-  document.getElementById('status').innerHTML = "You are not logged in into facebook, please log in to take a quiz";
-};
-}
-
 
 
 //part 4:
-function howMany(selectObject) {var numberSelected = 0
-for (var i = 0, i < selectObject.options.length, i++) {if
-  (selectObject.options[i].selected) {
+function howMany(selectObject, otherThing, moreThings) {
+  var numberSelected = 0;
+  for (var i = 0; i < selectObject.options.length; i++) {
+    if (selectObject.options[i].selected) {
       numberSelected++;
-  }
-  }
-return numberSelected;
-}
-
-
-//part 5:
-      let userInfo = {fname: req.body.fname,
-        lname: req.body.lname,
-        email: email,
-        passwordDigest: hash
     }
-
-
-
-
-//part 6:
-if( $('#check').html().length +1  === $('#userPick').html().length ) {
-       $winnermsg.append('Congratulation you won')
-      newValue ++;
-       $(".balance").text(newValue);
-
   }
-     else {
-       newValue -=1;
-       $(".balance").text(newValue);
-
-     };
-
+  return numberSelected;
 }
 
-//
+
+// part 5:
+// es5
+var userInfo = {
+  fname: req.body.fname,
+  lname: req.body.lname,
+  email: email,
+  passwordDigest: hash
+};
+
+// es6
+/* blurg */
+const userInfo = {
+  fname: req.body.fname,
+  lname: req.body.lname,
+  email: email,
+  passwordDigest: hash,
+};
 
 
+// part 6:
+var newValue = 0;
+var $balance = $('.balance');
 
-
-
-
-
-
-
-
-
-
+if (($('#check').html().length+1) === $('#userPick').html().length) {
+  $winnermsg.append('Congratulation you won');
+  newValue++;
+  $balance.text(newValue);
+} else {
+  newValue -= 1;
+  $balance.text(newValue);
+}
