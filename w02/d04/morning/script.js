985cf2f09a8ef22fc581006db6140ffd39711ced
mohamed gassama <bouyagassama@gmail.cpm>
2016-09-29 13:40:17 -0400

    hello

---
+++ w02/d04/morning/script.js
@@ -0,0 +1,19 @@
+console.log('connected !');
+
+var aboutMe = {};
+  aboutMe.firstName = 'Mohamed';
+  aboutMe.lastName = 'Gassama';
+  aboutMe.age = 27;
+  aboutMe.habbies = [
+  'Movie',
+  'Soccer',
+  'Football'
+];
+
+function printFirstLastName(person){
+  console.log (person.firstName +  person.lastName);
+}
+printFirstLastName(trevor);
+
+
+
