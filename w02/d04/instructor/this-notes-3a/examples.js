4f21e32c965d298a031568cae312b1cbdc6c5e9b
Jason Seminara <jseminara@gmail.com>
2016-09-30 12:43:55 -0400

    instructor examples

--- w02/d04/instructor/this-notes-3a/examples.js
+++ w02/d04/instructor/this-notes-3a/examples.js
@@ -1,5 +1,5 @@
 // Jason Seminara
-'use strict'
+
 // 1
 // What is the call-site of THIS?
 function foo(a){
@@ -7,3 +7,19 @@ function foo(a){
 }
 var a = 2;
 foo(88); // <---call site
+
+
+function setBPM(bpm){
+  this.bpm = bpm;
+}
+
+function getBPM(){
+  return this.bpm;
+}
+
+function Person(age, height){
+  this.age = age;
+  this.height = height;
+  this.setBpm =
+  this.getBpm = function(){ return this.bpm;}
+}
