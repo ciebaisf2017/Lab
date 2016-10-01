dff26d241e96e94b42cb84d981c8a6ee1a841b0e
mohamed gassama <bouyagassama@gmail.cpm>
2016-10-01 14:35:05 -0400

    js

--- w02/d05/morning/starter.js
+++ w02/d05/morning/starter.js
@@ -1,17 +1,80 @@
 console.log('javascript connected!')
 
-$(function(){
-  //build your functions here:
-  function flatten2(){
-    console.log('box2 is 200px');
-    $('#box2').css('height','200px')
-  }
+  var hoverCounter = 0
+  var doubleCounter = 0
+
+function flatten2(){
+  console.log('box2 is 200px');
+  $('#box1').on('mouseover', function(){
+    $('#box2').css('height', '200px');
+  });
+}
+
+
+function mod1() {
+  console.log('box1 is flattening');
+  $('#box2').on('mouseover', function() {
+   if (hoverCounter < 4){
+    console.log(hoverCounter);
+    $('#box1').css('height', .8*($('#box1').height()));
+      hoverCounter++
+   }else{
+    hoverCounter = 0;
+    $('#box1').css('height', 2*($('#box1').height()));
+   }
+  });
+}
+
+
+function bigger1(){
+  $('#box1').on('click', function(){
+     console.log('box1 just grew 20%');
+    $('#box1').css('height', 1.2*($('#box1').height()));
+  });
+}
+
+function colorSwap() {
+  $('#box1').on('mousedown', function(){
+    console.log('color change !');
+    $('#box1').css('background-color', 'navy');
+    $('#box2').css('background-color', 'maroon');
+  });
+}
+
+function roundCorners1() {
+  $('#box1').on('mouseup', function() {
+     console.log('box 1 corner rounded');
+    $('#box1').animate({'border-radius':'20%'}, '200');
+  });
+}
 
 
 
+function box2Huge() {
+  $('#box2').on('dblclick', function(){
+    if(doubleCounter < 3){
+      console.log('CLICK MORE');
+      doubleCounter++
+    }else{
+    console.log('2 GREW TO GIANT PROPORTIONS!');
+     $('#box2').css({
+        'height':'100vh',
+        'width':'100vw',
+        'top':'0px',
+        'left':'0px'
+      });
+    }
+  });
+}
 
-  //apply your event listners here:
-  $('#box1').mouseover(flatten2);
+
+$(document).ready(function() {
+  flatten2();
+  mod1();
+  bigger1();
+  colorSwap();
+  roundCorners1();
+  box2Huge();
+});
 
 
-})
