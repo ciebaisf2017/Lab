cbb19c1fd943ff328d487ae05038de63962fc845
Irwin Tsay <irwintsay@gmail.com>
2016-10-13 01:30:49 -0400

    update starter code and remove solutions

--- w04/d04/morning/part2/js/scroll.js
+++ w04/d04/morning/part2/js/scroll.js
@@ -1,54 +1,9 @@
-//scrolling function
-function scrollTo (element, top, duration) {
-  if (duration < 0) { 
-    return
-  };
-  
-  //get the different between target and current position on the HTML page
-  var difference = top - element.scrollTop;
-
-  var perTick = difference / duration * 10;
-  
-  setTimeout(function() {
-    element.scrollTop = element.scrollTop + perTick;
-    
-    //exit out of the setTimeout if reach our target position 
-    if (element.scrollTop == top) {
-      return
-    };
-    
-    //put our function into itself but with decrease duration so when duration is 0 zero
-    scrollTo(element, top, duration - 10);
-    //setTimeout wait 10 mill-seconds
-  }, 10);
-};
-
-function scrollToJQ (element, top, duration) {
-
-  if (duration < 0) {
-    return;
-  }
-
-  let difference = top - element.scrollTop();
-  let perTick = difference / duration * 10;
-
-  setTimeout(function() {
-    element.scrollTop(element.scrollTop() + perTick);
-
-    if (element.scrollTop() === top) {
-      return;
-    }
-
-    scrollToJQ(element, top, duration - 10);
-  }, 10);
-}
-
-function scrollToJQInterval(element, top, duration) {
+function scrollTo(element, top, duration) {
 
   let distance = top - element.scrollTop();
   let perTick = distance / duration * 10;
 
-  let clearScroll = setInterval(function() {
+  let clearScroll = setInterval( () => {
     if (duration < 0 || element.scrollTop === top) {
       clearInterval(clearScroll);
     } else {
