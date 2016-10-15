9decd806a4b329be78f8e14bee7b544bb01b38fc
mohamed gassama <bouyagassama@gmail.cpm>
2016-10-15 13:36:02 -0400

    mars

--- w04/d04/morning/part2/js/launch.js
+++ w04/d04/morning/part2/js/launch.js
@@ -14,5 +14,37 @@ console.log('Mission to Mars is a go!');
 // Attach the click event to the Go! button
 
 
+const countdown = () => {
+  alert('Rocket is ready to launch!');
+  let timer = 10;
+  let counterDisplay = $('#count');
+  counterDisplay.text(timer);
+  let clearTimer = setInterval(() => {
+    timer--;
+    if (timer === 0) {
+      clearInterval(clearTimer);
+      scrollTo($('body'), 0, 5000)
+    }
+    counterDisplay.text(timer);
+  }, 100);
+}
 
+const goButtonHandler = () => {
+  let temp, fuel, pwd;
+  temp = parseInt(prompt('Temperature in F'));
+  fuel = parseInt(prompt('Fuel percentage'));
+  pwd = prompt('Enter password');
+  if (temp <= 200 && fuel > 60 && pwd === 'fizzbuzz') {
+    countdown();
+  }else{
+    alert("We can't go to the Mars");
+  }
+
+}
+
+
+$(() => {
+  window.scrollBy(0, 20000);
+  $('#launch').click(goButtonHandler);
+});
 
