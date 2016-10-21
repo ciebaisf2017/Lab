5105b3f713f152145cc62048412ef8d3b75b93f8
mohamed gassama <bouyagassama@gmail.cpm>
2016-10-21 10:11:13 -0400

    update

--- w05/d01/classwork/express_101/app.js
+++ w05/d01/classwork/express_101/app.js
@@ -9,32 +9,16 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.use(logger('dev'));
-
+app.use(express.static('public'));
 // listening the port
 app.listen(port, () => console.log('server is listening on port', port))
 
 // event
 
 app.get('/', (req, res) => {
-  const links = ['/about', '/contact'];
    res.render('index', {
     message: 'This is the homepage',
-    links: links
    });
 });
 
-app.get('/about', (req, res) => {
-  const links = ['/', '/contact']
-  res.render('index' ,{
-    message: 'this is about page',
-    links: links
-  });
-});
 
-app.get('/contact', (req, res) => {
-  const links = ['/', '/about'];
- res.render( 'index' , {
-  message: 'this is contact page',
-  links: links
- });
-})
