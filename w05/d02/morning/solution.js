cf692122fcf030c85f553cd63fe2fd8cf8a18645
Trevor Preston <trevorpreston4@gmail.com>
2016-10-21 09:00:03 -0400

    w5d5 morn ex

--- w05/d02/morning/solution.js
+++ w05/d02/morning/solution.js
@@ -32,34 +32,34 @@ var frameworks = {
   ] // ends backendArray
 }
 
-// 1.1
-frameworks.frontEnd.forEach((framework) => {
-  let newSentence = framework.name + ' is ' + framework.description;
-  console.log(newSentence);
-})
+// // 1.1
+// frameworks.frontEnd.forEach((framework) => {
+//   let newSentence = framework.name + ' is ' + framework.description;
+//   console.log(newSentence);
+// })
 
 
-// 1.2
-const randomNumbers = [3,5,2,8,1];
-let total = 0;
-randomNumbers.forEach((number) => {
-  total += number;
-});
-console.log(total);
+// // 1.2
+// const randomNumbers = [3,5,2,8,1];
+// let total = 0;
+// randomNumbers.forEach((number) => {
+//   total += number;
+// });
+// console.log(total);
 
 
-// 2.1
-let modifiedFrontEnd = frameworks.frontEnd.map((framework) => {
-  return framework.name + '.js';
-});
-console.log(modifiedFrontEnd);
+// // 2.1
+// let modifiedFrontEnd = frameworks.frontEnd.map((framework) => {
+//   return framework.name + '.js';
+// });
+// console.log(modifiedFrontEnd);
 
 
-// 2.2
-let modifiedBackEnd = frameworks.backEnd.map((framework) => {
-  return framework.name + '.js';
-});
-console.log(modifiedBackEnd);
+// // 2.2
+// let modifiedBackEnd = frameworks.backEnd.map((framework) => {
+//   return framework.name + '.js';
+// });
+// console.log(modifiedBackEnd);
 
 
 // 3.1
@@ -70,18 +70,24 @@ let filteredFrontEnd = frameworks.frontEnd.filter((framework) => {
 });
 console.log(filteredFrontEnd);
 
-
-// 3.2
-let filteredBackEnd = frameworks.backEnd.filter((framework) => {
-  if (framework.description.length > 30) {
-    return framework;
+let filteredByState = sightings.filter((stateSightings) = >{
+  if(stateSightings.state === req.query.state){
+    return stateSightings
   }
-})
-console.log(filteredBackEnd);
+}
 
 
-// 4.1
-let frontEndNames = frameworks.frontEnd.reduce((sum, framework) => {
-  return sum += framework.name;
-},'');
-console.log(frontEndNames);
+// // 3.2
+// let filteredBackEnd = frameworks.backEnd.filter((framework) => {
+//   if (framework.description.length > 30) {
+//     return framework;
+//   }
+// })
+// console.log(filteredBackEnd);
+
+
+// // 4.1
+// let frontEndNames = frameworks.frontEnd.reduce((sum, framework) => {
+//   return sum += framework.name;
+// },'');
+// console.log(frontEndNames);
