33ed1ddb1c31f27b9b0f8e87f6c5a96f80bc3085
mohamed gassama <bouyagassama@gmail.cpm>
2016-11-15 09:06:10 -0500

    hello

--- w09/d01/morning/clock.js
+++ w09/d01/morning/clock.js
@@ -1,5 +1,19 @@
 const clock = () => {
-
+  const minuteHand = document.querySelector('#minute-hand');
+  const secondHand = document.querySelector('#second-hand');
+  const hourdHand = document.querySelector('#hour-hand');
+  let secondInt = 0;
+  let minuteInt = 0;
+  let hourInt = 0;
+  setInterval( () =>{
+   console.log('time passing');
+   secondInt += 6;
+   minuteInt += .1
+   hourInt += .1 / 12;
+   secondHand.style.transform  = `rotate(${secondInt}deg)`
+   minuteHand.style.transform = `rotate(${minuteInt}deg)`
+   hourdHand.style.transform = `rotate(${hourInt}deg)`
+  }, 1000)
 };
 
 document.addEventListener('DOMContentLoaded', () => {
